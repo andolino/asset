4b1b5c0eeb2c6d0171a4f29dee593b4a16fa8b97
dondonpentavia <baisac.andolino@gmail.com>
2020-10-20 18:28:18 +0800

    db up

---
+++ steller_db.sql
@@ -0,0 +1,486 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 20, 2020 at 10:51 AM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `steller_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `brand`
+--
+
+CREATE TABLE `brand` (
+  `brand_id` int(14) NOT NULL,
+  `brand_name` varchar(128) DEFAULT NULL,
+  `brand_status` enum('ACTIVE','INACTIVE') NOT NULL DEFAULT 'INACTIVE'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `cat_id` int(14) NOT NULL,
+  `cat_name` varchar(64) DEFAULT NULL,
+  `cat_status` enum('ACTIVE','INACTIVE') DEFAULT 'INACTIVE'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `color`
+--
+
+CREATE TABLE `color` (
+  `color_id` int(14) NOT NULL,
+  `color_name` varchar(64) DEFAULT NULL,
+  `color_status` enum('ACTIVE','INACTIVE') NOT NULL DEFAULT 'INACTIVE'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `groups`
+--
+
+CREATE TABLE `groups` (
+  `id` int(14) NOT NULL,
+  `group_name` varchar(128) DEFAULT NULL,
+  `description` varchar(512) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notes`
+--
+
+CREATE TABLE `notes` (
+  `id` int(14) NOT NULL,
+  `user_id` varchar(128) DEFAULT NULL,
+  `comment_id` varchar(64) DEFAULT NULL,
+  `title` varchar(256) DEFAULT NULL,
+  `description` varchar(512) DEFAULT NULL,
+  `note_image` varchar(128) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `datetime` varchar(128) DEFAULT NULL,
+  `notification_status` enum('seen','unseen') NOT NULL DEFAULT 'unseen'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+CREATE TABLE `product` (
+  `id` int(14) NOT NULL,
+  `pro_id` varchar(64) DEFAULT NULL,
+  `cat_id` int(14) DEFAULT NULL,
+  `subcat_id` int(14) DEFAULT NULL,
+  `brand_id` int(14) DEFAULT NULL,
+  `pro_sku` varchar(64) DEFAULT NULL,
+  `pro_name` varchar(256) DEFAULT NULL,
+  `pro_price` varchar(64) DEFAULT NULL,
+  `selling_price` varchar(64) DEFAULT NULL,
+  `discount` varchar(64) DEFAULT NULL,
+  `discount_starts` varchar(64) DEFAULT NULL,
+  `discount_end` varchar(64) DEFAULT NULL,
+  `pro_image` varchar(64) DEFAULT NULL,
+  `pro_image1` varchar(128) DEFAULT NULL,
+  `pro_image2` varchar(128) DEFAULT NULL,
+  `pro_image3` varchar(128) DEFAULT NULL,
+  `pro_summery` varchar(512) DEFAULT NULL,
+  `pro_details` varchar(1024) DEFAULT NULL,
+  `quantity` varchar(64) DEFAULT NULL,
+  `date` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product_color`
+--
+
+CREATE TABLE `product_color` (
+  `id` int(14) NOT NULL,
+  `pro_id` varchar(64) DEFAULT NULL,
+  `color_id` int(14) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product_image`
+--
+
+CREATE TABLE `product_image` (
+  `id` int(14) NOT NULL,
+  `pro_id` varchar(128) DEFAULT NULL,
+  `img_url` varchar(256) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product_size`
+--
+
+CREATE TABLE `product_size` (
+  `id` int(14) NOT NULL,
+  `pro_id` varchar(64) DEFAULT NULL,
+  `size_id` int(14) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `settings`
+--
+
+CREATE TABLE `settings` (
+  `id` int(11) NOT NULL,
+  `sitelogo` varchar(128) DEFAULT NULL,
+  `sitetitle` varchar(256) DEFAULT NULL,
+  `description` varchar(512) DEFAULT NULL,
+  `copyright` varchar(128) DEFAULT NULL,
+  `contact` varchar(128) DEFAULT NULL,
+  `currency` varchar(128) DEFAULT NULL,
+  `symbol` varchar(64) DEFAULT NULL,
+  `system_email` varchar(128) DEFAULT NULL,
+  `address` varchar(256) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `settings`
+--
+
+INSERT INTO `settings` (`id`, `sitelogo`, `sitetitle`, `description`, `copyright`, `contact`, `currency`, `symbol`, `system_email`, `address`) VALUES
+(1, 'design2.png', 'Codeigniter crud with ajax for admin and product management', 'Codeigniter crud with ajax for admin and product management site or any other site', 'madCoderz', '324324234234', 'USD', '$', 'hello@demo.com', 'Makon lake view 3234');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `size`
+--
+
+CREATE TABLE `size` (
+  `size_id` int(14) NOT NULL,
+  `size_name` varchar(64) DEFAULT NULL,
+  `size_status` enum('ACTIVE','INACTIVE') NOT NULL DEFAULT 'INACTIVE'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `social_link`
+--
+
+CREATE TABLE `social_link` (
+  `id` int(14) NOT NULL,
+  `user_id` varchar(128) DEFAULT NULL,
+  `facebook` varchar(256) DEFAULT NULL,
+  `twitter` varchar(256) DEFAULT NULL,
+  `google_plus` varchar(256) DEFAULT NULL,
+  `skype` varchar(256) DEFAULT NULL,
+  `flicker` varchar(256) DEFAULT NULL,
+  `youtube` varchar(256) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sub_category`
+--
+
+CREATE TABLE `sub_category` (
+  `subcat_id` int(14) NOT NULL,
+  `cat_id` int(14) DEFAULT NULL,
+  `subcat_name` varchar(64) DEFAULT NULL,
+  `subcat_status` enum('ACTIVE','INACTIVE') NOT NULL DEFAULT 'INACTIVE'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `to_do_list`
+--
+
+CREATE TABLE `to_do_list` (
+  `id` int(14) NOT NULL,
+  `user_id` varchar(64) DEFAULT NULL,
+  `to_dodata` varchar(256) DEFAULT NULL,
+  `date` varchar(128) DEFAULT NULL,
+  `value` varchar(14) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(14) NOT NULL,
+  `user_id` varchar(64) DEFAULT NULL,
+  `full_name` varchar(128) DEFAULT NULL,
+  `email` varchar(256) DEFAULT NULL,
+  `password` varchar(512) DEFAULT NULL,
+  `ip_address` varchar(512) DEFAULT NULL,
+  `forgotten_code` varchar(512) DEFAULT NULL,
+  `address` varchar(512) DEFAULT NULL,
+  `dob` varchar(128) DEFAULT NULL,
+  `image` varchar(128) DEFAULT NULL,
+  `contact` varchar(256) DEFAULT NULL,
+  `gender` enum('MALE','FEMALE') NOT NULL DEFAULT 'MALE',
+  `country` varchar(128) DEFAULT NULL,
+  `created_on` varchar(128) DEFAULT NULL,
+  `status` enum('ACTIVE','INACTIVE') NOT NULL DEFAULT 'INACTIVE',
+  `user_type` enum('User','Admin') NOT NULL DEFAULT 'User',
+  `confirm_code` varchar(128) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `user_id`, `full_name`, `email`, `password`, `ip_address`, `forgotten_code`, `address`, `dob`, `image`, `contact`, `gender`, `country`, `created_on`, `status`, `user_type`, `confirm_code`) VALUES
+(10, 'U36', 'Adalweng', 'admin@admin.com', '6367c48dd193d56ea7b0baad25b19455e529f5ee', '::1', 'bfc958008879d88d8075e76e188e44ee', 'Altstadt, 06108 Halle (Saale), Germany', '15/05/1992', 'U362.jpg', '08333533545', 'MALE', 'Germany', '10/27/2017', 'ACTIVE', 'Admin', '1660'),
+(12, 'U4', 'mardyon', 'mardyon.yongson1026@gmail.com', '6367c48dd193d56ea7b0baad25b19455e529f5ee', '::1', NULL, '307 Magnolia Street Lake View Homes, Brgy Putatan', '10/26/1992', NULL, '09275006357', 'MALE', 'Philippines', '10/18/2020', 'ACTIVE', 'Admin', '1952113897');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_group`
+--
+
+CREATE TABLE `user_group` (
+  `id` int(14) NOT NULL,
+  `user_id` varchar(128) DEFAULT NULL,
+  `group_id` int(14) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `brand`
+--
+ALTER TABLE `brand`
+  ADD PRIMARY KEY (`brand_id`);
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`cat_id`);
+
+--
+-- Indexes for table `color`
+--
+ALTER TABLE `color`
+  ADD PRIMARY KEY (`color_id`);
+
+--
+-- Indexes for table `groups`
+--
+ALTER TABLE `groups`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `notes`
+--
+ALTER TABLE `notes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `product_color`
+--
+ALTER TABLE `product_color`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `product_image`
+--
+ALTER TABLE `product_image`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `product_size`
+--
+ALTER TABLE `product_size`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `settings`
+--
+ALTER TABLE `settings`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `size`
+--
+ALTER TABLE `size`
+  ADD PRIMARY KEY (`size_id`);
+
+--
+-- Indexes for table `social_link`
+--
+ALTER TABLE `social_link`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `sub_category`
+--
+ALTER TABLE `sub_category`
+  ADD PRIMARY KEY (`subcat_id`);
+
+--
+-- Indexes for table `to_do_list`
+--
+ALTER TABLE `to_do_list`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_group`
+--
+ALTER TABLE `user_group`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `brand`
+--
+ALTER TABLE `brand`
+  MODIFY `brand_id` int(14) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `cat_id` int(14) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `color`
+--
+ALTER TABLE `color`
+  MODIFY `color_id` int(14) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `groups`
+--
+ALTER TABLE `groups`
+  MODIFY `id` int(14) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `notes`
+--
+ALTER TABLE `notes`
+  MODIFY `id` int(14) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+  MODIFY `id` int(14) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `product_color`
+--
+ALTER TABLE `product_color`
+  MODIFY `id` int(14) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `product_image`
+--
+ALTER TABLE `product_image`
+  MODIFY `id` int(14) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `product_size`
+--
+ALTER TABLE `product_size`
+  MODIFY `id` int(14) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `settings`
+--
+ALTER TABLE `settings`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `size`
+--
+ALTER TABLE `size`
+  MODIFY `size_id` int(14) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `social_link`
+--
+ALTER TABLE `social_link`
+  MODIFY `id` int(14) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `sub_category`
+--
+ALTER TABLE `sub_category`
+  MODIFY `subcat_id` int(14) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `to_do_list`
+--
+ALTER TABLE `to_do_list`
+  MODIFY `id` int(14) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(14) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `user_group`
+--
+ALTER TABLE `user_group`
+  MODIFY `id` int(14) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
